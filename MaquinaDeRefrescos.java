c9949c1c5c242799f9984fcf2f401517f199cf22
Nelson Mamani <nelson.mercer.1@gmail.com>
2023-08-18 06:00:47 -0400

    v2.0 - Correción de un bug en el atributo caja.

--- MaquinaDeRefrescos.java
+++ MaquinaDeRefrescos.java
@@ -2,43 +2,46 @@
 
 public class MaquinaDeRefrescos{
     
+    //atributos
     private int costo;
     private int cantidad;
     private int caja;
     
+    //constructor
     public MaquinaDeRefrescos(int costo, int cantidad, int caja){
         this.costo = costo;
         this.cantidad = cantidad;
         this.caja = caja;
     }
     
+    
+    //getters
     public int getCosto(){
         return this.costo;
     }
-    
     public int getCantidad(){
         return this.cantidad;
     }
-    
     public int getCaja(){
         return this.caja;
     }
     
+    //setters
     public void setCosto(int costo){
         this.costo = costo;
     }
-    
     public void setCantidad(int cantidad){
         this.cantidad = cantidad;
     }
-    
     public void setCaja(int caja){
         this.caja = caja;
     }
     
+    //metodos
     public void solicitarRefresco(int dinero){
         if(dinero == getCosto()){
             entregarRefresco();
+            setCaja(getCaja() + dinero);
         }
     }
     
